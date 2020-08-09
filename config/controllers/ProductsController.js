95dd88c6eeb4418211d7aff81e7800f2d0a5c8dd
Dcox270891 <55593346+Dcox270891@users.noreply.github.com>
2020-08-09 18:23:27 +0100

    Added hot and new to products for home page carousel

--- config/controllers/ProductsController.js
+++ config/controllers/ProductsController.js
@@ -40,5 +40,17 @@ module.exports ={
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+    getNewProducts: function(req, res){
+      db.Products
+        .find({ new: true, })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    getHotProducts: function(req, res){
+      db.Products
+        .find({ hot : true, })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
 
 }
